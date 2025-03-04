3b431e6a8f93af57dcd55d4cea4363f2e1291317
Bogdan Rebeles <rebelesbogdan.c.f@gmail.com>
2025-03-04 18:58:45 +0200

    Created module using gradle.

---
+++ StartGradle/src/test/java/ComputerRepairRequestTest.java
@@ -0,0 +1,24 @@
+import ro.mpp2025.model.ComputerRepairRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ComputerRepairRequestTest {
+    @Test
+    @DisplayName("First Test")
+    public void firstTest() {
+        ComputerRepairRequest computerRepairRequest = new ComputerRepairRequest();
+        assertEquals("", computerRepairRequest.getOwnerName());
+        assertEquals("", computerRepairRequest.getOwnerAddress());
+    }
+
+    @Test
+    @DisplayName("Second Test")
+    public void secondTest() {
+        assertEquals(2, 2, "Numerele ar trebui sa fie egale!");
+        //assertEquals(2, 3, "Numerele ar trebui sa fie egale!");
+    }
+      
+}
